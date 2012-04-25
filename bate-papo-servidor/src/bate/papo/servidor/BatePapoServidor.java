9b821868f1a808cece73cd29d8106e528c202126
Rafael <jrtadf@gmail.com>
2012-04-24 22:19:25 -0300

    Implementação inicial do servidor

--- bate-papo-servidor/src/bate/papo/servidor/BatePapoServidor.java
+++ bate-papo-servidor/src/bate/papo/servidor/BatePapoServidor.java
@@ -1,8 +1,8 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package bate.papo.servidor;
+
+import java.io.DataInputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 /**
  *
@@ -10,10 +10,42 @@ package bate.papo.servidor;
  */
 public class BatePapoServidor {
 
-    /**
-     * @param args the command line arguments
-     */
+    ServerSocket server;
+    //final int port = 5588;
+    final int port = 8080;
+
+    public BatePapoServidor() {
+        System.out.println("Iniciando Servidor");
+        try {
+            this.server = new ServerSocket(this.port);
+            System.out.println("Servidor iniciado na porta: "+this.port);
+            this.escutar();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    
+    
+    public Socket escutar() {
+        Socket cliente = null;
+        try{
+            System.out.println("Servidor aguardando clientes...");
+            cliente = this.server.accept();
+            System.out.println("Cliente conectado");
+            System.out.println("IP: "+cliente.getInetAddress().getHostAddress());
+            System.out.println("Hostname: "+cliente.getInetAddress().getHostName());
+
+            DataInputStream entrada = new DataInputStream(cliente.getInputStream());
+            while(true)
+            System.out.println(entrada.readUTF());
+
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        return cliente;
+    }
+
     public static void main(String[] args) {
-        // TODO code application logic here
+        new BatePapoServidor();
     }
 }
